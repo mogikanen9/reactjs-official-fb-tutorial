75612849c399e5c2e52c1f1e8c631deeb3ca1cc1
Vlad Voinov <mogikanen9@users.noreply.github.com>
2017-09-30 13:52:13 -0400

    Tic tac toe (#2)
    * state wasmoved to Board component out of Square
    
    * Square component was reimplemented with functional style
    
    * X and O swithc logic implemented

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-    
-constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
-    render() {
-      return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
-    }
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
   
   class Board extends React.Component {
 
@@ -27,7 +17,17 @@ constructor() {
         super();
         this.state = {
           squares: Array(9).fill(null),
+          xIsNext: true
         };
+      }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';;
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
       }
 
     renderSquare(i) {
@@ -37,7 +37,7 @@ constructor() {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
